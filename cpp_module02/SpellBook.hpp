a46a782eb15726332929394e16518bf3a3d114ad
ahkalama <116187665+ahkalama@users.noreply.github.com>
2024-06-12 13:48:53 +0300

    Update SpellBook.hpp

--- cpp_module02/SpellBook.hpp
+++ cpp_module02/SpellBook.hpp
@@ -1,17 +1,17 @@
 #pragma once
-#include "ASpell.hpp"
+#include <iostream>
 #include <map>
+#include "ASpell.hpp"
 
 class SpellBook
 {
-	private :
-		SpellBook(SpellBook const & src);
-		SpellBook & operator=(SpellBook const & src);
-		std::map < std::string, ASpell*> _SpellBook;
-	public :
-		SpellBook();
-		~SpellBook();
-		void learnSpell(ASpell*);
-		void forgetSpell(std::string const &);
-		ASpell* createSpell(std::string const &);
+    private:
+        std::map<std::string, ASpell *> _spellbook;
+
+    public:
+        SpellBook();
+        ~SpellBook();
+        void learnSpell(ASpell*spell);
+        void forgetSpell(std::string const &spell);
+        ASpell* createSpell(std::string const &spell);
 };
