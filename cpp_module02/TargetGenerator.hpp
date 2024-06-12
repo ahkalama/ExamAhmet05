52693f272c457f1892587539785cd0eff3c0578a
ahkalama <116187665+ahkalama@users.noreply.github.com>
2024-06-12 13:49:12 +0300

    Update TargetGenerator.hpp

--- cpp_module02/TargetGenerator.hpp
+++ cpp_module02/TargetGenerator.hpp
@@ -1,17 +1,17 @@
 #pragma once
-#include "ATarget.hpp"
+#include <iostream>
 #include <map>
+#include "ATarget.hpp"
 
 class TargetGenerator
 {
-	private :
-		TargetGenerator(TargetGenerator const & src);
-		TargetGenerator & operator=(TargetGenerator const & src);
-		std::map < std::string, ATarget*> _target;
-	public :
-		TargetGenerator();
-		~TargetGenerator();
-		void learnTargetType(ATarget*);
-		void forgetTargetType(std::string const &);
-		ATarget* createTarget(std::string const &);
+    private:
+        std::map<std::string, ATarget *> _target;
+
+    public:
+        TargetGenerator();
+        ~TargetGenerator();
+        void learnTargetType(ATarget*spell);
+        void forgetTargetType(std::string const &spell);
+        ATarget* createTarget(std::string const &spell);
 };
